a211c648fa8c57eeb254282ecbc117d62a9b755d
Konstantin BIFERT <hello@kissu.io>
2018-02-20 23:33:59 +0100

    Basic HP system done, can now fight !

--- 046 Introduction  Challenge/js/app.js
+++ 046 Introduction  Challenge/js/app.js
@@ -1,19 +1,44 @@
-var player = new Vue({
-  el: '#player',
+var vm = new Vue({
+  el: '#app',
   data: {
-    playerProgressBar: {
+    playerHealth: 80,
+    foeHealth: 75,
+    gameOver: false,
+    littleCSS: {
       backgroundColor: 'teal',
-      width: '70%'
+    }
+  },
+  methods: {
+    newGame: function() {
+      this.gameOver = false,
+      this.playerHealth = 80,
+      this.foeHealth = 75
+    },
+    updateHealth: function (value, event) {
+      if (this.foeHealth > 0 && this.playerHealth > 0) {
+        this.playerHealth = this.playerHealth - getRandomIntInclusive(value, value * 3)
+        this.foeHealth = this.foeHealth - getRandomIntInclusive(value, value * 4)
+      } else {
+        alert('Game Over')
+        this.gameOver = true
+      }
+    },
+    healPlayer: function (value) {
+      this.playerHealth = this.playerHealth - getRandomIntInclusive(value, value * 3)
+      this.playerHealth = this.playerHealth + getRandomIntInclusive(value, value * 4)
+    },
+    giveUp: function() {
+      alert('Loser !')
+      this.playerHealth = 0,
+      this.foeHealth = 100,
+      this.gameOver = true
     }
   }
 })
 
-var foe = new Vue({
-  el: '#foe',
-  data: {
-    foeProgressBar: {
-      backgroundColor: 'red',
-      width: '30%'
-    }
-  }
-})
+function getRandomIntInclusive(min, max) {
+  // this is not Vue :O !! I know... :3
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
