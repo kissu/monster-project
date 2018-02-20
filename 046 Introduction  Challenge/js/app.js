acb6bf4bb57da7710504d728963f7ba8266e3566
Konstantin BIFERT <hello@kissu.io>
2018-02-21 00:33:15 +0100

    Hm, first project done

--- 046 Introduction  Challenge/js/app.js
+++ 046 Introduction  Challenge/js/app.js
@@ -4,6 +4,7 @@ var vm = new Vue({
     playerHealth: 80,
     foeHealth: 75,
     gameOver: false,
+    logs: [],
     littleCSS: {
       backgroundColor: 'teal',
     }
@@ -16,22 +17,27 @@ var vm = new Vue({
     },
     updateHealth: function (value, event) {
       if (this.foeHealth > 0 && this.playerHealth > 0) {
-        this.playerHealth = this.playerHealth - getRandomIntInclusive(value, value * 3)
-        this.foeHealth = this.foeHealth - getRandomIntInclusive(value, value * 4)
+        this.playerHealth -= getRandomIntInclusive(value, value * 3)
+        this.logs.unshift('Player is hit')
+        this.foeHealth -= getRandomIntInclusive(value, value * 4)
+        this.logs.unshift("Let's defeat the monster !")
       } else {
         alert('Game Over')
         this.gameOver = true
+        this.logs.unshift('Game is done')
       }
     },
     healPlayer: function (value) {
-      this.playerHealth = this.playerHealth - getRandomIntInclusive(value, value * 3)
-      this.playerHealth = this.playerHealth + getRandomIntInclusive(value, value * 4)
+      if (this.playerHealth < 90) this.playerHealth += getRandomIntInclusive(value, value * 4)
+      this.playerHealth -= getRandomIntInclusive(value, value * 3)
+      this.logs.unshift('Player is desesperatly trying to heal himself while enemy is kicking his @$$')
     },
     giveUp: function() {
       alert('Loser !')
       this.playerHealth = 0,
       this.foeHealth = 100,
-      this.gameOver = true
+      this.gameOver = true,
+      this.logs = []
     }
   }
 })
